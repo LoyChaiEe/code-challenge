6575e25645deb643ee340549bfb87da66a9aeb1a
LoyChaiEe <celoy001@gmail.com>
2023-09-11 15:47:16 +0800

    added search bar component, reading how it is implemented

--- src/problem2/frontend/src/components/Form.jsx
+++ src/problem2/frontend/src/components/Form.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Searchbar from "./Serach";
 
 const Form = () => {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("");
   return (
     <form onsubmit="return !1">
       <h5>Swap</h5>
       <label for="input-amount">Amount to send</label>
       <input id="input-amount" />
+      <Searchbar/>
 
       <label for="output-amount">Amount to receive</label>
       <input id="output-amount" />
+      <Searchbar/>
 
       <button>CONFIRM SWAP</button>
     </form>
